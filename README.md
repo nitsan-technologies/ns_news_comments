147fb96052f1c575839c6da2238cadd3af1fb2c5
NITSAN <sanjay@nitsan.in>
2019-11-13 15:30:24 +0530

    [TODO] Documentation Update!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ns_news_comments
-The TYPO3 Extension: https://extensions.typo3.org/extension/ns_news_comments/
+
+- FREE version: You can download FREE version with basic-features at https://extensions.typo3.org/extension/ns_news_comments/
+- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/ns-news-comments-pro/
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
     'title' => '[NITSAN] Comment Plugin for EXT:news',
-    'description' => 'Do you want to smoothly integrate comment feature in your favourite News extension? In this extension, there are features like post comment, reply to comment, multi-level comment thread, moderation etc., Know more from extension manual.',
+    'description' => 'Do you want to smoothly integrate comment features in your favourite News extension? This extension includes powerful features like post comment, reply to comment, multi-level comment thread, moderation etc., You can download PRO version for more-features & free-support at https://t3terminal.com/ns-news-comments-pro/',
     'category' => 'plugin',
     'author' => 'T3:Bhavin Barad, T3:Keval Pandya, FE:Mehul Nimavat, QA:Vandna Kalivada',
     'author_email' => 'sanjay@nitsan.in',
@@ -11,11 +11,11 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '4.0.0',
+    'version' => '4.0.1',
     'constraints' => array(
         'depends' => array(
             'typo3' => '6.2.0-9.5.99',
-            'news' => '3.0.0-7.9.9',
+            'news' => '3.0.0-7.9.99',
         ),
         'conflicts' => array(
         ),

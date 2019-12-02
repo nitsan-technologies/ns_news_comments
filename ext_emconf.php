ea76f6317a922e9f39c642bc797159c7e8c68ecd
NITSAN <sanjay@nitsan.in>
2019-12-02 18:31:50 +0530

    [TODO] Adding Gitlab-CI & Travis with PHP, TypoScript, YAML lints [TODO] Updating documentation [TODO] Added demo link [TODO] Releasing pro-version of the extenion at https://t3terminal.com/

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,8 @@
 <?php
+
 $EM_CONF[$_EXTKEY] = array(
     'title' => '[NITSAN] Comment Plugin for EXT:news',
-    'description' => 'Do you want to smoothly integrate comment features in your favourite News extension? This extension includes powerful features like post comment, reply to comment, multi-level comment thread, moderation etc., You can download PRO version for more-features & free-support at https://t3terminal.com/ns-news-comments-pro/',
+    'description' => 'Do you want to smoothly integrate comment features in your favourite News extension? This extension includes powerful features like post comment, reply to comment, multi-level comment thread, moderation etc., Demo: https://demo.t3terminal.com/t3t-extensions/news-comment/ You can download PRO version for more-features & free-support at https://t3terminal.com/ns-news-comments-pro/',
     'category' => 'plugin',
     'author' => 'T3:Bhavin Barad, T3:Keval Pandya, FE:Mehul Nimavat, QA:Vandna Kalivada',
     'author_email' => 'sanjay@nitsan.in',
@@ -11,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '4.0.1',
+    'version' => '4.0.2',
     'constraints' => array(
         'depends' => array(
             'typo3' => '6.2.0-9.5.99',

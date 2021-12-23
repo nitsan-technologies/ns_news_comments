2aef143b536b43507dd17a475bf73b0b7dd01ea1
NITSAN Technologies <sanjay@nitsan.in>
2021-12-23 20:00:01 +0530

    [TASK] Correct the demo page URL

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ns_news_comments'] = [
     'title' => '[NITSAN] Comment Plugin for EXT:news',
-    'description' => 'Do you want to smoothly integrate comment features in your favourite News extension? This extension includes powerful features like post comment, reply to comment, multi-level comment thread, moderation etc., Demo: https://demo.t3terminal.com/t3t-extensions/news-comment/ You can download PRO version for more-features & free-support at https://t3terminal.com/ns-news-comments-pro/',
+    'description' => 'Do you want to smoothly integrate comment features in your favourite News extension? This extension includes powerful features like post comment, reply to comment, multi-level comment thread, moderation etc., Demo: https://demo.t3terminal.com/t3t-extensions/news-comments/ You can download PRO version for more-features & free-support at https://t3terminal.com/news-comment-typo3-extension/',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',

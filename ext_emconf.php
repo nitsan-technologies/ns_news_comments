09b790f54c3082bccf9c5903f5010fd0308ae325
NITSAN <sanjay@nitsan.in>
2025-06-24 11:55:30 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_news_comments'] = [
-    'title' => 'News Comment',
-    'description' => 'All New TYPO3 News Comment Extension helps add comments to your news pages and articles. It comes with a variety of features for managing and customizing comments. It is designed to enhance user experience with a simple interface and is SEO-friendly.
+    'title' => 'TYPO3 News Comments Extension',
+    'description' => 'Add a full-featured comment system to your TYPO3 News articles. This extension provides easy comment management, a clean frontend interface, and SEO-friendly functionality to improve user engagement on your news pages.',
 
-    *** Live Demo: https://demo.t3planet.com/t3-extensions/seo/news-comments/ *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-news-comment-extension',
     'category' => 'plugin',
-    'author' => 'T3: Himanshu Ramavat, T3: Divya Goklani, T3: Nilesh Malankiya, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
